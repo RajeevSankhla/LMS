11930440af472bf50af30682da5f4831fa8ed469
RajeevSankhla <35911596+RajeevSankhla@users.noreply.github.com>
2020-08-05 21:26:42 +0530

    Add files via upload

---
+++ lout.php
@@ -0,0 +1,21 @@
+    <?php
+      // If the user is logged in, delete the session vars to log them out
+      session_start();
+      if (isset($_SESSION['user'])) {
+        // Delete the session vars by clearing the $_SESSION array
+        $_SESSION = array();
+     
+        // Delete the session cookie by setting its expiration to an hour ago (3600)
+        if (isset($_COOKIE[session_name()])) {      setcookie(session_name(), '', time() - 3600);    }
+     
+        // Destroy the session
+        session_destroy();
+      }
+     
+      // Delete the user ID and username cookies by setting their expirations to an hour ago (3600)
+      setcookie('user', '', time() - 3600);
+      setcookie('pass', '', time() - 3600);
+     
+      // Redirect to the home page
+      header("Location:login.html");
+    ?>
