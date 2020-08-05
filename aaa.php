ac96280074f9f7d96982c2ac56a43f37449aa070
Anit Pratap <67693241+Anit-Pratap@users.noreply.github.com>
2020-08-05 22:07:07 +0530

    Add files via upload

---
+++ aaa.php
@@ -0,0 +1,11 @@
+<html>  
+<body bgcolor="pink">  
+<h3>Format of Date</h3>  
+<?php  
+echo "First Format  : ". date ("d/m/Y") . "<br />";  
+echo "Second Format  :".date ("d.m.Y") . "<br />";  
+echo "Third Format  :".date ("d-m-Y");  
+?>   
+</body>  
+</html>  
+
